ce4048f88821674f5d50b90abd129c7c4d7b69b7
rgruncio <rgruncio@gmail.com>
2017-10-23 20:49:01 +0200

    0.6 - opracowane wyszukiwanie przystanków oraz przeszukiwanie rozkładu jazdy

--- app/src/main/java/com/rgruncio/nextbus/Main.java
+++ app/src/main/java/com/rgruncio/nextbus/Main.java
@@ -6,14 +6,25 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.ToggleButton;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 
 public class Main extends AppCompatActivity {
@@ -21,10 +32,16 @@ public class Main extends AppCompatActivity {
     private BroadcastReceiver gpsBcastRcvr;
     private BroadcastReceiver sqlBcastRcvr;
     private BroadcastReceiver usrBcastRcvr;
+    private BroadcastReceiver busStopRcvr;
+    private BroadcastReceiver busInfoRcvr;
     private TextView textView;
-    private TextView responseTextView;
+    private TextView busStopNameTextView;
+    private TextView lineNumberTextView;
+    private TextView busTimeTextView;
+    private LinearLayout infoLayout;
     private Context context;
     private Intent loginIntent = null;
+    private Location actual;
 
     @Override
     protected void onResume() {
@@ -44,7 +61,8 @@ public class Main extends AppCompatActivity {
                     try {
                         latitude = intent.getExtras().get("latitude").toString();
                         longitude = intent.getExtras().get("longitude").toString();
-                        textView.setText("Device position:\nlongitude: " + longitude + "\nlatitude: " + latitude);
+                        actual.setLatitude(Double.parseDouble(latitude));
+                        actual.setLongitude(Double.parseDouble(longitude));
                     } catch (NullPointerException ex) {
                         ex.printStackTrace();
                     }
@@ -59,13 +77,12 @@ public class Main extends AppCompatActivity {
                     sqlIntent.putExtra(SQLService.QUERY, SQLService.FIND_NEAREST_BUS_STOP);
 
                     startService(sqlIntent);
-
                 }
             };
         }
         registerReceiver(gpsBcastRcvr, new IntentFilter(GPSService.INTENT_FILTER));
 
-        if (sqlBcastRcvr == null){
+        if (sqlBcastRcvr == null) {
             sqlBcastRcvr = new BroadcastReceiver() {
 
                 @Override
@@ -74,15 +91,92 @@ public class Main extends AppCompatActivity {
 
                     sqlResponse = intent.getStringExtra(SQLService.RESPONSE);
                     String text = "DB Response:\n" + sqlResponse;
-                    responseTextView.setText(text);
+                    //responseTextView.setText(text);
                 }
             };
         }
         registerReceiver(sqlBcastRcvr, new IntentFilter(SQLService.QUERY));
         registerReceiver(sqlBcastRcvr, new IntentFilter(SQLService.FIND_USER));
 
+        if (busStopRcvr == null) {
+            busStopRcvr = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    String sqlResponse;
 
-        if (usrBcastRcvr == null){
+                    sqlResponse = intent.getStringExtra(SQLService.RESPONSE);
+
+                    //busTextView.setText(sqlResponse);
+                    try {
+                        JSONArray json = new JSONArray(sqlResponse);
+
+                        String id = json.getJSONObject(0).getString("ID");
+                        String lat = json.getJSONObject(0).getString("Latitude");
+                        String lon = json.getJSONObject(0).getString("Longitude");
+
+                        Location busStopLocation = new Location(LOCATION_SERVICE);
+                        busStopLocation.setLatitude(Double.parseDouble(lat));
+                        busStopLocation.setLongitude(Double.parseDouble(lon));
+
+                        if (actual.distanceTo(busStopLocation) < 15) {
+
+                            textView.setVisibility(View.INVISIBLE);
+
+                            DateFormat df = new SimpleDateFormat("EEEyyyyMMdd");
+                            df.setTimeZone(TimeZone.getTimeZone("Europe/Warsaw"));
+                            String date = df.format(Calendar.getInstance().getTime());
+                            int typeOfDay = whichDay(date);
+
+                            Intent _intent = new Intent(getApplicationContext(), SQLService.class);
+                            _intent.putExtra("ID", id);
+                            _intent.putExtra("TypeOfDay", typeOfDay);
+                            _intent.putExtra(SQLService.QUERY, SQLService.GET_BUSSTOP_INFO);
+
+                            startService(_intent);
+                        }
+                        else
+                        {
+                            infoLayout.setVisibility(View.INVISIBLE);
+                            textView.setVisibility(View.VISIBLE);
+                            textView.setText("Brak przystanku w odległości 15 metrów.");
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+        }
+        registerReceiver(busStopRcvr, new IntentFilter(SQLService.FIND_NEAREST_BUS_STOP));
+
+        if (busInfoRcvr == null) {
+            busInfoRcvr = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    String response = intent.getExtras().getString(SQLService.RESPONSE);
+
+                    try {
+                        JSONArray json = new JSONArray(response);
+                        String busStopName = json.getJSONObject(0).getString("Name");
+                        String lineNumber = json.getJSONObject(0).getString("Line_Number");
+                        String time = json.getJSONObject(0).getString("TIME");
+
+                        busStopNameTextView.setText(busStopName);
+                        lineNumberTextView.setText(lineNumber);
+                        busTimeTextView.setText(time);
+
+                        infoLayout.setVisibility(View.VISIBLE);
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            };
+        }
+        registerReceiver(busInfoRcvr, new IntentFilter(SQLService.GET_BUSSTOP_INFO));
+
+        if (usrBcastRcvr == null) {
             usrBcastRcvr = new BroadcastReceiver() {
                 @Override
                 public void onReceive(Context context, Intent intent) {
@@ -92,9 +186,7 @@ public class Main extends AppCompatActivity {
                     if (correct) {
                         TextView tvLogin = (TextView) findViewById(R.id.loginTextView);
                         tvLogin.setText(login);
-                    }
-                    else
-                    {
+                    } else {
                         startActivity(loginIntent);
                     }
                 }
@@ -119,6 +211,9 @@ public class Main extends AppCompatActivity {
         if (usrBcastRcvr != null) {
             unregisterReceiver(usrBcastRcvr);
         }
+        if (busStopRcvr != null) {
+            unregisterReceiver(busStopRcvr);
+        }
     }
 
     //Funkcja wywoływana przy uruchamianiu poraz pierwszy aplikacji (tworzenie UI i inicjalizacja obiektów)
@@ -127,19 +222,103 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        actual = new Location(LOCATION_SERVICE);
         context = this.getApplicationContext();
 
         textView = (TextView) findViewById(R.id.textView);
 
-        responseTextView = (TextView) findViewById(R.id.resposeTextView);
+        busStopNameTextView = (TextView) findViewById(R.id.busNameTextView);
+        lineNumberTextView = (TextView) findViewById(R.id.lineNumberTextView);
+        busTimeTextView = (TextView) findViewById(R.id.busTimeTextView);
+        infoLayout = (LinearLayout) findViewById(R.id.infoLayout);
+        infoLayout.setVisibility(View.INVISIBLE);
 
-        loginIntent = new Intent(this, Login.class);
-        startActivity(loginIntent);
+
+
+        //loginIntent = new Intent(this, Login.class);
+        //startActivity(loginIntent);
 
         //sprawdzamy czy aplikacja ma uprawnienia do korzystania z usługi GPS
         if (!runtime_permissions())
             enable_buttons();
 
+    }
+
+    //sprawdzanie jaki jest dzień: powszedni, sobota, czy święto.
+    // 0 - dzień powszedni
+    // 1 - sobota
+    // 2 - święto
+
+    // date - format: EEEyyyyMMdd
+    private int whichDay(String date) {
+
+        String dayName = date.substring(0, 3);
+        int year = Integer.parseInt(date.substring(3, 7));
+        int month = Integer.parseInt(date.substring(7, 9));
+        int day = Integer.parseInt(date.substring(9, 11));
+
+        switch (dayName) {
+            case "Sun":
+                return 2;
+            default:
+                switch (month) {
+                    case 1:
+                        if (day == 1 || day == 6) return 2;
+                    case 5:
+                        if (day == 1 || day == 3) return 2;
+                    case 8:
+                        if (day == 15) return 2;
+                    case 11:
+                        if (day == 1 || day == 11) return 2;
+                    case 12:
+                        if (day == 25 || day == 26) return 2;
+                    default:
+                        if (dayName.equals("Sat")) return 1;
+                        else {
+                            int A = 24;
+                            int B = 5;
+                            int a = year % 19;
+                            int b = year % 4;
+                            int c = year % 7;
+                            int d = (a * 19 + A) % 30;
+                            int e = (2 * b + 4 * c + 6 * d + B) % 7;
+                            if (d == 29 && e == 6) d -= 7;
+                            if (d == 28 && e == 6) d -= 7;
+                            int easter = 22 + d + e;
+                            int easterMonth = 3;
+                            if (easter > 31) {
+                                easterMonth++;
+                                easter = easter % 31;
+                            }
+                            //Wielkanoc
+                            if (day == easter && month == easterMonth) return 2;
+                            //Poniedziałek Wielkanocny
+                            if (day - 1 == easter && month == easterMonth) return 2;
+                            //Zesłanie Ducha Swiętego (49 dni po Wielkanocy) to zawsze niedziela
+                            //ten przypadek jest sprawdzany na początku
+                            //Sprawdzamy datę Bożego Ciała (60 dni po Wielkanocy)
+                            {
+                                int dd = 0;
+                                int lim;
+                                while (dd < 60) {
+                                    dd++;
+                                    easter++;
+                                    if (easterMonth == 4) lim = 30;
+                                    else lim = 31;
+                                    if (easter > lim) {
+                                        easterMonth++;
+                                        easter -= lim;
+                                    }
+                                }
+
+                                if (day == easter && month == easterMonth)
+                                    return 2;
+                                else
+                                    return 0;
+                            }
+                        }
+                }
+        }
     }
 
     //aktywacja widgetów
@@ -184,7 +363,6 @@ public class Main extends AppCompatActivity {
         }
         return false;
     }
-
 
     //funkcja pobierająca aktualne uprawnienia do usługi GPS
     @Override
--- app/src/main/java/com/rgruncio/nextbus/SQLService.java
+++ app/src/main/java/com/rgruncio/nextbus/SQLService.java
@@ -1,12 +1,11 @@
 package com.rgruncio.nextbus;
 
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
 import android.os.AsyncTask;
 import android.os.IBinder;
 import android.support.annotation.NonNull;
-import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,8 +15,12 @@ import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TimeZone;
 
 public class SQLService extends Service {
 
@@ -25,9 +28,13 @@ public class SQLService extends Service {
 
     public final static String FIND_USER = "FIND_USER";
 
-    public final static String FIND_NEAREST_BUS_STOP = "";
+    public final static String FIND_NEAREST_BUS_STOP = "FIND_NEAREST";
 
-    public final static String DO_NOTHING = "SELECT * FROM Przystanki WHERE ID_Prz=0;";
+    public final static String GET_BUSSTOP_INFO = "GET_BUSSTOP_INFO";
+
+    public final static String GET_BUSES = "GET_BUSES";
+
+    public final static String DO_NOTHING = "SELECT * FROM Przystanki WHERE ID_Prz=1;";
 
     public final static String EMAIL = "email";
 
@@ -39,6 +46,7 @@ public class SQLService extends Service {
 
     public final static String LATITUDE = "latitude";
 
+    private Location actual;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -55,24 +63,54 @@ public class SQLService extends Service {
         //Odpytujemy bazę w zależności od żadania jakie przyszło w parametrze QUERY
 
         String _query = intent.getStringExtra(QUERY);
+        String query;
 
-        switch (_query)
-        {
+        switch (_query) {
             case FIND_USER:
                 String mEmail = intent.getStringExtra(EMAIL);
 
-                String query = "SELECT * FROM USERS WHERE email='" + mEmail + "';";
+                query = "SELECT * FROM USERS WHERE email='" + mEmail + "';";
 
                 //Toast.makeText(getApplicationContext(), query, Toast.LENGTH_LONG).show();
                 new _SQLQuery().execute(FIND_USER, query);
 
                 break;
             case FIND_NEAREST_BUS_STOP:
+                String latitude = intent.getExtras().get(LATITUDE).toString();
+                String longitude = intent.getExtras().get(LONGITUDE).toString();
+                Double lat = Double.parseDouble(latitude);
+                Double lon = Double.parseDouble(longitude);
+
+                query = "SELECT * FROM `NB_BUS_STOPS` WHERE `Latitude` <= '" + (lat + 0.001) + "' AND `Longitude` <= '" + (lon + 0.001) +
+                        "' ORDER BY `Latitude` DESC LIMIT 1";
+
+                //Toast.makeText(getApplicationContext(), query, Toast.LENGTH_LONG).show();
+
+                new _SQLQuery().execute(FIND_NEAREST_BUS_STOP, query);
+
                 //TODO: algorytm wyszukiwania najbliższego przystanku
+                break;
+
+            case GET_BUSES:
+
+                break;
+
+            case GET_BUSSTOP_INFO:
+                String id = intent.getExtras().get("ID").toString();
+                String typeOfDay = intent.getExtras().get("TypeOfDay").toString();
+                DateFormat df = new SimpleDateFormat("HH:mm:ss");
+                df.setTimeZone(TimeZone.getTimeZone("Europe/Warsaw"));
+                String time = df.format(Calendar.getInstance().getTime());
+
+                query = "SELECT NB_BUS_STOPS.Name, NB_BUSES.Line_Number, NB_BUSES.TIME FROM NB_BUS_STOPS, NB_BUSES" +
+                        " WHERE NB_BUS_STOPS.ID = NB_BUSES.BS_ID AND NB_BUS_STOPS.ID = " + id +
+                        " AND NB_BUSES.DAY = " + typeOfDay +
+                        " AND TIME(NB_BUSES.TIME) > '" + time + "' ORDER BY NB_BUSES.TIME LIMIT 1";
+
+                new _SQLQuery().execute(GET_BUSSTOP_INFO, query);
                 break;
             default:
                 String sqlQuery = intent.getStringExtra("query");
-                //Toast.makeText(getApplicationContext(), sqlQuery, Toast.LENGTH_LONG).show();
                 new _SQLQuery().execute(QUERY, sqlQuery);
                 break;
         }
@@ -105,7 +143,7 @@ public class SQLService extends Service {
                 connection.connect();
 
                 OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
-                HashMap<String,String> sql = new HashMap<>();
+                HashMap<String, String> sql = new HashMap<>();
                 sql.put("query", params[1]);
                 out.write(getPostDataString(sql));
                 out.close();
@@ -147,7 +185,7 @@ public class SQLService extends Service {
         private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
             StringBuilder result = new StringBuilder();
             boolean first = true;
-            for(Map.Entry<String, String> entry : params.entrySet()){
+            for (Map.Entry<String, String> entry : params.entrySet()) {
                 if (first)
                     first = false;
                 else
@@ -171,10 +209,10 @@ public class SQLService extends Service {
         /**
          * Funkcja rozgłaszająca Intent z zadanym parametrem, z filtrem <i>type</i>
          *
-         * @param type rodzaj wykonanego zapytania
+         * @param type     rodzaj wykonanego zapytania
          * @param response łańcuch znaków do przesłania przez Intent
          */
-        private void sendIntent(String type, String response){
+        private void sendIntent(String type, String response) {
 
             Intent i = new Intent(type);
             i.putExtra(RESPONSE, response);
